1922322f36091019fadc8c15a526fc5a001bbd42
Mior Muhammad Zaki <crynobone@gmail.com>
2022-02-23 16:52:02 +0800

    wip

--- mix.js
+++ mix.js
@@ -1,5 +1,6 @@
 const mix = require('laravel-mix')
 const webpack = require('webpack')
+const path = require('path')
 
 class NovaExtension {
   name() {
@@ -21,6 +22,14 @@ class NovaExtension {
   webpackConfig(webpackConfig) {
     webpackConfig.externals = {
       vue: 'Vue',
+    }
+
+    webpackConfig.resolve.alias = {
+      ...(webpackConfig.resolve.alias || {}),
+      'laravel-nova': path.join(
+        __dirname,
+        'vendor/laravel/nova/resources/js/mixins/packages.js'
+      ),
     }
 
     webpackConfig.output = {
